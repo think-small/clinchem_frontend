ed262be8e519bade961c4d653562817bd806408f
Barry Small <Barry.T.Small@gmail.com>
2020-11-07 13:28:06 -0600

    Add modal for selecting units

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { NavLink } from "react-router-dom";
-import { Button } from "react-materialize";
+import { Modal, Button, Checkbox } from "react-materialize";
 import "materialize-css";
 import "./sidebar.styles.scss";
 import { ReactComponent as Clipboard } from "../../assets/zondicons/clipboard.svg";
@@ -13,9 +13,39 @@ const Sidebar: FC = () => {
   return (
     <aside className="sidebar">
       <section className="case-selector">
-        <Button node="button" waves="light" className="case-selector-button">
-          Select Unit
-        </Button>
+          <Modal
+              actions={[
+                  <Button flat modal="close" node="button" waves="green">Close</Button>
+              ]}
+              bottomSheet={false}
+              fixedFooter={false}
+              header="Unit Selection"
+              id="Modal-0"
+              open={false}
+              options={{
+                  dismissible: true,
+                  endingTop: '10%',
+                  inDuration: 250,
+                  opacity: 0.5,
+                  outDuration: 250,
+                  preventScrolling: true,
+                  startingTop: '4%'
+              }}
+              trigger={<Button node="button">Select Unit</Button>}
+          >
+              <p>
+                  Select the units you wish to review
+              </p>
+              <form>
+                  <div className="unit-selection-group">
+                      <Checkbox value={"OsmoAndLytes"} label={"Osmolality and Electrolytes"} />
+                      <Checkbox value={"Renal"} label={"Renal Function"} />
+                      <Checkbox value={"Lipids"} label={"Lipid Metabolism"} />
+                      <Checkbox value={"HIVAndHepatitis"} label={"HIV and Viral Hepatitis"} />
+                      <Checkbox value={"Enzymes"} label={"Enzymes"} />
+                  </div>
+              </form>
+          </Modal>
       </section>
       <NavLink className="sidebar-item" to="/notes" activeClassName="isActive">
         <span>Transcriptions</span>
